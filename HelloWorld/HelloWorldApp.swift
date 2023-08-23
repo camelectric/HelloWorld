a5a3311223af45296ae4a8c1a904cd15b7bb060a
Philippe Landry <philippe.landry.montreal@gmail.com>
2023-08-23 15:35:33 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Philippe Landry on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
